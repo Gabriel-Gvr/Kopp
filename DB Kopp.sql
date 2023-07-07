a35a0cfbe2b96563e307fd3734c8c511051246e0
Gabriel-Gvr <85135553+Gabriel-Gvr@users.noreply.github.com>
2023-07-07 04:06:40 -0300

    Update DB Kopp.sql

--- DB Kopp.sql
+++ DB Kopp.sql
@@ -15,4 +15,18 @@ CREATE TABLE "Infracao" (
   "valida" BOOLEAN
 );
 
+SELECT *
+FROM Infracao
+WHERE velocidade_medida >= 0.2 * velocidade_permitida
+ORDER BY data_hora;
+
+SELECT Remessa.id, Remessa.status, Remessa.total_infractions,
+       COUNT(CASE WHEN Infracao.valida THEN 1 END) AS total_validas,
+       COUNT(CASE WHEN NOT Infracao.valida THEN 1 END) AS total_invalidas
+FROM Remessa
+LEFT JOIN Infracao ON Remessa.id = Infracao.remessa_id
+GROUP BY Remessa.id, Remessa.status
+ORDER BY Remessa.status;
+
+
 ALTER TABLE "Infracao" ADD FOREIGN KEY ("remessa_id") REFERENCES "Remessa" ("id");
